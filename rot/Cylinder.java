a6db005294d2bf08c065628a48bab5717316bf25
Attila Csölle <127153544+acsolle66@users.noreply.github.com>
2023-09-28 14:14:50 +0200

    Initialization of repository

---
+++ rot/Cylinder.java
@@ -0,0 +1,44 @@
+package rot;
+
+import java.util.LinkedList;
+import rot.utils.Alphabet;
+
+/*
+ *@author	Attila Csölle 
+ *
+ */
+
+public class Cylinder {
+	private LinkedList<Character> cylinderChars = new LinkedList<>();
+	
+	public Cylinder() {
+		this(0);
+	}
+	
+	public Cylinder(int rot) {
+		shiftCylinder(rot);
+	}
+	
+	private void shiftCylinder(int rot) {
+		char[] abc = Alphabet.ABC;
+		int start = 0;
+		int end = abc.length;
+		
+		// Initializing the base list with unshifted letters
+		while (start < end) {
+			cylinderChars.add(abc[start]);
+			start++;
+		}
+		
+		// Shifting the position of lettres according to the defined ROT
+		for (int i = 0; i < rot; i++) {
+			cylinderChars.add(cylinderChars.remove());
+		}
+		
+	}
+	
+	public LinkedList<Character> readCylinder() {
+		return this.cylinderChars;
+	}
+	
+}
---
+++ rot/utils/Alphabet.java
@@ -0,0 +1,21 @@
+package rot.utils;
+
+public class Alphabet {
+	public static char[] ABC = generateABC();
+	
+	private static char[] generateABC() {
+		char[] abc = new char[26];
+		int start = 97;
+		int end = 122;
+		int charIndex = 0; 
+		
+		while (start <= end) {
+			abc[charIndex] = (char)start;
+			start++;
+			charIndex++;
+		}
+		
+		return abc;
+	}
+	
+}

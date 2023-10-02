842f212cae36eac5928cc7432fbd63546d78ddc7
acsolle66 <csolle.coding@gmail.com>
2023-10-02 15:29:02 +0200

    Refractored Alphabet.java - declared named constants for magick numbers - renamed class to EnglishAlphabetGenerator - removed the static modifier of method generateABC - changed the name of method generateABC to getABC - formatted the code

--- rot/cipher/Cipher.java
+++ rot/cipher/Cipher.java
@@ -2,48 +2,26 @@ package rot.cipher;
 import java.lang.StringBuilder;
 import java.util.LinkedList;
 
-     /**
-	  * Class for representing the cylinder object with the shifted character set  
-	  * @author		Attila Csölle
-	  * @version 	1.0
-      * @since		09.29.2023
-	 **/
 public class Cipher {
 	private Cylinder baseCylinder;
 	private LinkedList<Character> baseChars;
 	private Cylinder shiftedCylinder;
 	private LinkedList<Character> shiftedChars;
 	
-	/**
-	  * Constructor of the Cipher class
-	  * Instantiates the cipher object with two cylinders,
-	  * one Cylinder with unshifted character set,
-	  * and one with shifted character set.
-	  * If the mode is set to encryption the base charset is unshifted, so the cipherText method translates the text forward 
-	  * If the mode is set to decryption the base charset is shifted, so the cipherText method translates the text backward 
-	  * @param rot Integer value of the character shift
-	  * @param mode String value of the cipher mode, encrypt or decrypt
-	 **/
 	public Cipher(int rot, String mode) {
 		if (mode.equals("e")) {
 			baseCylinder = new Cylinder();
-			baseChars = baseCylinder.readCylinder();
+			baseChars = baseCylinder.readCylinderCharset();
 			shiftedCylinder = new Cylinder(rot);
-			shiftedChars = shiftedCylinder.readCylinder();	
+			shiftedChars = shiftedCylinder.readCylinderCharset();	
 		} else {
 			baseCylinder = new Cylinder(rot);
-			baseChars = baseCylinder.readCylinder();
+			baseChars = baseCylinder.readCylinderCharset();
 			shiftedCylinder = new Cylinder();
-			shiftedChars = shiftedCylinder.readCylinder();	
+			shiftedChars = shiftedCylinder.readCylinderCharset();	
 		}
 	}
 	
-    /**
-	  * Method for encrypt/decrypt the text
-	  * Depending on the instantiated object the method translates forward or backward the text
-	  * @param text Text to cipher
-	  * @return Returns the ciphered text
-	 **/
 	public String cipherText(String text) {
 		
 		char[] letters = text.toLowerCase().toCharArray();
---
+++ rot/cipher/EnglishAlphabetGenerator.java
@@ -0,0 +1,24 @@
+package rot.cipher;
+
+class EnglishAlphabetGenerator {
+  private final int LETTER_A;
+  private final int LETTER_Z;
+
+  EnglishAlphabetGenerator() {
+    LETTER_A = 97;
+    LETTER_Z = 122;
+  }
+
+  char[] getABC() {
+    char[] ABC = new char[26];
+    int start = LETTER_A;
+    int end = LETTER_Z;
+    int charIndex = 0;
+    while (start <= end) {
+      ABC[charIndex] = (char) start;
+      start++;
+      charIndex++;
+    }
+    return ABC;
+  }
+}
